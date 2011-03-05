b0b2cc63917a08f5cb93febf504efa97c40d65bd
Roald Frederickx <roald.frederickx@gmail.com>
2011-03-05 22:21:54 +0100

    initial Square class, implemented temperature

---
+++ Square.java
@@ -0,0 +1,120 @@
+/* 
+ * Author     : Roald Frederickx
+ * License    : GPL
+ */
+
+
+public class Square {
+
+	//TODO: Farenheit?
+
+	/**
+	 * Returns the temperature of this Square.
+	 *
+	 * @return The temperature of this Square in degrees Celcius.
+	 */
+	public double getTemperature() {
+		return temperature;
+	}
+
+	/**
+	 * Sets the temperature of this Square.
+	 *
+	 * @param temperature
+	 * The new temperature in degrees Celcius.
+	 * @throws IllegalArgumentException
+	 * This square can not have the given temperature.
+	 *   | !isValidTemperature(temperature)
+	 */
+	public void setTemperature(double temperature)
+			throws IllegalArgumentException {
+		if (! isValidTemperature(temperature))
+			throw new IllegalArgumentException();
+		this.temperature = temperature;
+	}
+
+	/** 
+	 * Checks whether the given temperature is valid for this Square. 
+	 * 
+	 * @param temperature
+	 * The temperature of this square in degrees Celcius.
+	 * @return
+	 * True iff the given temperature is not strictly lower than the lower 
+	 * temperature bound, and not strictly higher than the upper 
+	 * temperature bound.
+	 *   | return == (getMinTemperature() &lt;= temperature)
+	 *   |			&amp;&amp; (temperature &lt;= getMaxTemperature());
+	 */
+	public boolean isValidTemperature(double temperature){
+		return (getMinTemperature() <= temperature)
+				&& (temperature <= getMaxTemperature());
+	}
+	
+	/** 
+	 * Variable registering the current temperature of this square.
+	 */
+	private double temperature;
+
+
+	/** 
+	 * Set the temperature boundaries for this square. 
+	 * 
+	 * @param min
+	 * The lower temperature bound for this Square in degrees Celcius.
+	 * @param max
+	 * The upper temperature bound for this Square in degrees Celcius.
+	 * @throws IllegalArgumentException
+	 * The given temperature boundaries are illegal,
+	 *   | ! areValidTemperatureBoundaries(min, max)
+	 */
+	public void setTemperatureBoundaries(double min, double max)
+			throws IllegalArgumentException {
+		if (! areValidTemperatureBoundaries(min, max))
+			throw new IllegalArgumentException();
+	}
+
+	/** 
+	 * Check whether the given boundaries are valid temperature boundaries.
+	 * @param min
+	 * The lower temperature bound for this Square in degrees Celcius.
+	 * @param max
+	 * The upper temperature bound for this Square in degrees Celcius.
+	 * @return
+	 * True iff the lower bound is less or equal than the upper bound.
+	 *   | result == (min &lt;= max)
+	 */
+	public boolean areValidTemperatureBoundaries(double min, double max){
+		return min <= max;
+	}
+
+	/**
+	 * Returns the minimum temperature for this square.
+	 *
+	 * @return The minimum temperature in degrees Celcius..
+	 */
+	public double getMinTemperature() {
+		return minTemperature;
+	}
+	
+	/**
+	 * Returns the maximum temperature for this square.
+	 *
+	 * @return The maximum temperature in degrees Celcius..
+	 */
+	public double getMaxTemperature() {
+		return maxTemperature;
+	}
+
+	/** 
+	 * Variable registering the lower limit of the temperature of this 
+	 * square. 
+	 */
+	private double minTemperature = -200; //XXX not tidy to initialize here?
+	/** 
+	 * Variable registering the upper limit of the temperature of this 
+	 * square. 
+	 */
+	private double maxTemperature = 5000; //XXX not tidy to initialize here?
+
+}
+

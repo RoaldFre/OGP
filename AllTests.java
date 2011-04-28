4b932a7d84de67ac6ebd23b6876cd09c766305b9
Roald Frederickx <roald.frederickx@gmail.com>
2011-04-28 22:43:54 +0200

    forgot to retab AllTests

--- AllTests.java
+++ AllTests.java
@@ -6,16 +6,19 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-	SquareTest.class,
-	BorderTest.class,
-	WallTest.class,
-	DoorTest.class,
-	OpenBorderTest.class,
-	TemperatureTest.class,
-	CoordinateTest.class,
-	CoordinateSystemTest.class,
-	DungeonTest.class,
-	CoupleTest.class,
+    SquareTest.class,
+    BorderTest.class,
+    WallTest.class,
+    DoorTest.class,
+    OpenBorderTest.class,
+    TemperatureTest.class,
+    CoordinateTest.class,
+    CoordinateSystemTest.class,
+    DungeonTest.class,
+    CoupleTest.class,
 })
 public class AllTests {
 }
+
+// vim: ts=4:sw=4:expandtab:smarttab
+

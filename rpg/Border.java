49c68b43c012a3cade47b89b1d96b3f9cc6d4d4a
Roald Frederickx <roald.frederickx@gmail.com>
2011-05-25 16:12:43 +0200

    thorough review of Border, fixed some inconsistencies

--- rpg/Border.java
+++ rpg/Border.java
@@ -10,7 +10,6 @@ import be.kuleuven.cs.som.annotate.*;
  * border cannot exist without being linked to at least one square (unless 
  * the border is terminated).
  * 
- *
  * @invar
  * No border has the same square on both sides.
  *   | hasNoDuplicateSquares()
@@ -27,15 +26,16 @@ public abstract class Border {
     
     /** 
      * Create a new border that is attached to the given square.
-     * Note that this will <b>not</b> initialize the bidirectional 
-     * association from the square to this border! It is your duty to make 
-     * that link with this newly created border.
-     * Hence, the newly object will still be raw!
      * 
      * @param square 
      * The square to attach this border to.
      * @post
-     *   | bordersOnSquare(square)
+     *   | new.bordersOnSquare(square)
+     * @note
+     * Note that this will <b>not</b> initialize the bidirectional 
+     * association from the square to this border! It is your duty to make 
+     * that link with this newly created border.
+     * Hence, the newly created border will still be raw!
      */
     @Raw
     Border(Square square) {
@@ -109,14 +109,15 @@ public abstract class Border {
      * This old border and the given old border get merged into a single 
      * new border that borders both squares associated with both old 
      * borders. That new border is the least open border of the old 
-     * borders.
+     * borders. The other border gets updated to that least open border.
      * @return
      * The new border that results from merging this old border and the 
      * given old border.
      * @throws BorderConstraintsException
      * Merging the borders would violate a border constraint.
      * @throws BorderMergeException
-     * One of the two borders is already shared by two squares.
+     * One of the two involved (non-equal) borders is already shared by two 
+     * squares.
      */
     Border mergeWith(Border other) throws BorderConstraintsException,
                                         BorderMergeException {
@@ -164,12 +165,12 @@ public abstract class Border {
     @Deprecated
     protected void equilibrateSquares() 
                         throws MergingTemperaturesViolatesLimitsException {
-        if (isSharedByTwoSquares()){
-            Square square1 = getASquare();
-            Square square2 = getNeighbour(square1);
-            square1.mergeTemperatures(square2);
-            square1.mergeHumidities(square2);
-        }
+        if (!isSharedByTwoSquares())
+            return;
+        Square square1 = getASquare();
+        Square square2 = getNeighbour(square1);
+        square1.mergeTemperatures(square2);
+        square1.mergeHumidities(square2);
     }
 
     /** 
@@ -179,15 +180,10 @@ public abstract class Border {
      * The square to check.
      * @pre
      *   | square != null
-     * @return 
-     * Whether or not this border borders on the given square. This is 
-     * false if this border is terminated.
      */
-    @Raw
+    @Basic
     public boolean bordersOnSquare(Square square) {
         assert square != null;
-        if (isTerminated())
-            return false;
         return squares.contains(square);
     }
 
@@ -204,13 +200,14 @@ public abstract class Border {
     }
 
     /**
-     * Returns an iterator of the squares along this border.
+     * Returns an iterable of the squares along this border.
      *
      * @pre
      *   | !isTerminated()
      */
     @Basic
     public Iterable<Square> getSquares() {
+        assert !isTerminated();
         return squares;
     }
 
@@ -227,6 +224,7 @@ public abstract class Border {
      * return the neighbour, or null if this border has only one assocated 
      * square (the given one).
      */
+    @Basic
     public Square getNeighbour(Square square) {
         assert !isTerminated();
         return squares.getPartner(square);
@@ -237,17 +235,20 @@ public abstract class Border {
      * 
      * @return 
      * If this border is terminated, or if this border is not shared by two 
-     * squares, then the result is false. Otherwise, the result is the 
-     * equality of the two associated squares of this border.
+     * squares, then the result is true. Otherwise, the result is the 
+     * negation of the equality of the two associated squares of this 
+     * border.
      */
+    @Raw
     public boolean hasNoDuplicateSquares() {
         if (isTerminated() || !isSharedByTwoSquares())
             return true;
-        Square square1 = getASquare();
-        Square square2 = getNeighbour(square1);
+        if (squares == null)
+            return true;
+        Square square1 = squares.getAnElement();
+        Square square2 = squares.getPartner(square1);
         return !square1.equals(square2);
     }
-
 
     /** 
      * Returns whether this border is shared by two squares. 
@@ -296,13 +297,14 @@ public abstract class Border {
      * 
      * @return 
      * True if this border is not terminated and all of the squares it 
-     * borders to also have this border as a border.
+     * borders to, also have this border as a border.
      */
+    @Raw
     public boolean hasProperSquares() {
         if (isTerminated())
             return (squares == null);
         
-        for (Square square : getSquares())
+        for (Square square : squares)
             if (!square.hasBorder(this))
                 return false;
         return true;
@@ -316,10 +318,11 @@ public abstract class Border {
      * True iff this border is not terminated or all of the squares it 
      * borders are not terminated.
      */
+    @Raw
     public boolean hasNoTerminatedSquares() {
-        if (isTerminated())
+        if (squares == null)
             return true;
-        for (Square square : getSquares())
+        for (Square square : squares)
             if (square.isTerminated())
                 return false;
         return true;
@@ -333,14 +336,13 @@ public abstract class Border {
      * @pre
      *   | !isSharedByTwoSquares()
      * @post
-     *   | getNeighbour(old.getASquare()).equals(square)
+     *   | new.getNeighbour(old.getASquare()).equals(square)
      */
     private void addSquare(Square square) {
         assert !isSharedByTwoSquares();
         squares.add(square);
     }
 
-        
     /** 
      * Variable referencing the square(s) that border(s) on this border.
      */
@@ -370,11 +372,14 @@ public abstract class Border {
         if (isTerminated())
             return;
 
-        for (Square square : getSquares())
-            assert !square.hasBorder(this);
-
-        squares = null;
         isTerminated = true;
+        
+        if (squares == null)
+            return;
+
+        for (Square square : squares)
+            assert !square.hasBorder(this);
+        squares = null;
     }
     
     /**
